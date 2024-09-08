f9b034da5483d617840fdd2c9e74b7188f95c4b3
Sigmond Kukla <sigkukla@gmail.com>
2024-09-08 11:29:08 -0400

    Removed all extraneous files

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -11,7 +11,7 @@ const router = createRouter({
                 {
                     path: '/',
                     name: 'crud',
-                    component: () => import('@/views/pages/Crud.vue')
+                    component: () => import('@/views/Crud.vue')
                 },
             ]
         },
