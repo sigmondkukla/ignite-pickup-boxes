e10462e64ae574d6758414561cdd83aa5947129b
Sigmond Kukla <sigkukla@gmail.com>
2024-09-17 17:15:42 -0400

    Add app, boxes, scanner, and shift_register functionality

---
+++ backend/app.py
@@ -0,0 +1,56 @@
+from flask import Flask, send_file, jsonify, request
+from flask_cors import CORS
+import os
+from dotenv import load_dotenv
+import database as db
+from boxes import Boxes
+
+SER_PIN = 11 # GPIO board pin connected to serial data of shift register
+SRCLK_PIN = 13 # GPIO board pin connected to the shift register clock pin
+RCLK_PIN = 15 # GPIO board pin connected to the storage register clock pin (latch pin)
+NUM_BOXES = 8 # Number of boxes in the system
+
+load_dotenv()
+app = Flask(__name__)
+app.secret_key = os.getenv("FLASK_SECRET_KEY")
+cors = CORS(app, resources={r"/api/*": {"origins": "*"}})
+
+database = db.Database()
+boxes = Boxes(NUM_BOXES, SER_PIN, SRCLK_PIN, RCLK_PIN)
+
+@app.route("/api", methods=["GET"])
+def api():
+    return f"Hello, world!"
+
+@app.route("/api/prints", methods=["GET", "POST", "PUT", "DELETE"])
+def prints():
+    if request.method == "GET":
+        return jsonify([print.to_dict() for print in database.get_prints()])
+    elif request.method == "POST":
+        data = request.json
+        database.create_print(data["print_number"], data["email"], data["code"], data["box_id"])
+        return jsonify({"success": True})
+    elif request.method == "PUT":
+        data = request.json
+        database.set_print_status(data["print_id"], data["status"])
+        return jsonify({"success": True})
+    elif request.method == "DELETE":
+        data = request.json
+        database.delete_print(data["print_id"])
+        return jsonify({"success": True})
+    
+@app.route("/api/scan", methods=["POST"])
+def scan():
+    data = request.json
+    print_data = database.get_print_by_code(data["code"])
+    if print_data is None:
+        print("Code not found")
+        return jsonify({"success": False})
+    # Open the box
+    boxes.open_box(print_data.box_id)
+    # Make the box available
+    box = database.get_box(print_data.box_id)
+    database.set_box_print_id(box.id, -1)
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
---
+++ backend/boxes.py
@@ -0,0 +1,33 @@
+import shift_register
+import time
+
+class Boxes():
+    def __init__(self, num_boxes, ser_pin, srclk_pin, rclk_pin):
+        self.ser_pin = ser_pin
+        self.srclk_pin = srclk_pin
+        self.rclk_pin = rclk_pin
+        self.num_boxes = num_boxes
+
+        self.sr = shift_register.ShiftRegister(self.ser_pin, self.srclk_pin, self.rclk_pin, num_boxes)
+        self.deactivate_all() # Ensure that no boxes just pop open randomly
+
+    def deactivate_all(self):
+        # Bitshifts a 1 from bit 0 to the number of boxes, then subtracts 1 to make everything below 1
+        # then writes that to the shift register
+        self.sr.write((1 << self.num_boxes) - 1)
+
+    def open_box(self, box_num):
+        if box_num < 0:
+            raise ValueError("Box number must be greater than or equal to 0")
+        if box_num >= self.num_boxes:
+            raise ValueError(f"Box number must be less than the number of boxes ({self.num_boxes})")
+        
+        all_deactivated = (1 << self.num_boxes) - 1
+        box_activated = (1 << box_num) ^ all_deactivated # Bitshifts a 1 from bit 0 to the desired box number, then XORs it with all_deactivated
+        
+        self.sr.write(box_activated) # Bitshifts a 1 from bit 0 to the desired box number
+        time.sleep(0.1) # Wait long enough for the solenoid to open fully
+        self.deactivate_all() # Deactivate all boxes
+
+    def cleanup(self):
+        self.sr.cleanup()
