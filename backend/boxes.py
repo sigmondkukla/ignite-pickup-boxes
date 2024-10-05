0632faa8a81ce32589f366da8cee3e9d2667d9f8
Sigmond Kukla <sigkukla@gmail.com>
2024-10-05 17:44:58 -0400

    Deactivate shift register code

--- backend/boxes.py
+++ backend/boxes.py
@@ -1,20 +1,23 @@
-import shift_register
+# import shift_register
 import time
+import RPi.GPIO as GPIO
 
 class Boxes():
-    def __init__(self, num_boxes, ser_pin, srclk_pin, rclk_pin):
-        self.ser_pin = ser_pin
-        self.srclk_pin = srclk_pin
-        self.rclk_pin = rclk_pin
+    def __init__(self, num_boxes, box_pins):
         self.num_boxes = num_boxes
+        self.box_pins = box_pins
 
-        self.sr = shift_register.ShiftRegister(self.ser_pin, self.srclk_pin, self.rclk_pin, num_boxes)
+        GPIO.setmode(GPIO.BOARD)
+        for pin in self.box_pins:
+            GPIO.setup(pin, GPIO.OUT)
+            GPIO.output(pin, GPIO.HIGH) # relay board is active low
+
+        # self.sr = shift_register.ShiftRegister(self.ser_pin, self.srclk_pin, self.rclk_pin, num_boxes)
         self.deactivate_all() # Ensure that no boxes just pop open randomly
 
     def deactivate_all(self):
-        # Bitshifts a 1 from bit 0 to the number of boxes, then subtracts 1 to make everything below 1
-        # then writes that to the shift register
-        self.sr.write((1 << self.num_boxes) - 1)
+        for pin in self.box_pins:
+            GPIO.output(pin, GPIO.HIGH)
 
     def open_box(self, box_num):
         if box_num < 0:
@@ -22,14 +25,9 @@ class Boxes():
         if box_num >= self.num_boxes:
             raise ValueError(f"Box number must be less than the number of boxes ({self.num_boxes})")
         
-        all_deactivated = (1 << self.num_boxes) - 1
-        box_activated = (1 << box_num) ^ all_deactivated # Bitshifts a 1 from bit 0 to the desired box number, then XORs it with all_deactivated
-        
-        self.sr.write(box_activated) # Bitshifts a 1 from bit 0 to the desired box number
-        print(time.time())
-        time.sleep(0.5) # Wait long enough for the solenoid to open fully
-        print(time.time())
-        self.deactivate_all() # Deactivate all boxes
+        GPIO.output(self.box_pins[box_num], GPIO.LOW)
+        time.sleep(0.5)
+        GPIO.output(self.box_pins[box_num], GPIO.HIGH)
 
     def cleanup(self):
-        self.sr.cleanup()
+        GPIO.cleanup()
