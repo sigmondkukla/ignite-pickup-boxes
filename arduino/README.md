31bc5021a130057cd948a996f9af09bedcb47ae9
Sigmond Kukla <sigkukla@gmail.com>
2024-10-24 11:41:51 -0400

    Add slave arduino code

---
+++ arduino/README.md
@@ -0,0 +1,4 @@
+# arduino
+
+The slave stack Arduinos act as serial I/O expanders for the master Raspberry Pi.
+They just need to pull a given box pin low when requested over serial to activate the relay and unlock the box.
--- arduino/src/main.cpp
+++ arduino/src/main.cpp
@@ -1,18 +1,42 @@
 #include <Arduino.h>
 
-// put function declarations here:
-int myFunction(int, int);
+const int box_pins[] = {2, 3, 4, 5, 6, 7, 8, 9};
 
-void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+void open(int);
+
+void setup()
+{
+  // immediately setup outputs and set all to 1 (because the relay is active low)
+  for (int i = 0; i < 8; i++)
+  {
+    pinMode(box_pins[i], OUTPUT);
+    digitalWrite(box_pins[i], 1);
+  }
+
+  // start serial communication
+  Serial.begin(9600);
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void loop()
+{
+  if (Serial.available() > 0) // if there was a message received
+  {
+    String line = Serial.readStringUntil('\n'); // get the full message (1 line)
+    int box = line.toInt();                     // convert to int for box number
+    open(box);                                  // actually open the box
+  }
 }
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+void open(int box)
+{
+  if (box < 0 || box > 7) // if the box number is invalid
+  {
+    // Serial.println("error");
+    // just ignore the request
+    return;
+  }
+
+  digitalWrite(box_pins[box - 1], 0); // set the pin to 0 to activate the relay (and box lock solenoid)
+  delay(500);                         // wait for half a second
+  digitalWrite(box_pins[box - 1], 1); // set the pin back to 1 to deactivate
 }
