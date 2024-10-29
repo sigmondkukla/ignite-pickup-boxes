aaa3c20d65b3b7bb6c96a4a9ab0078585c860323
Sigmond Kukla <sigkukla@gmail.com>
2024-10-29 11:42:52 -0400

    Add info card

--- frontend/src/layout/AppLayout.vue
+++ frontend/src/layout/AppLayout.vue
@@ -9,6 +9,47 @@ import AppTopbar from './AppTopbar.vue';
         <app-topbar></app-topbar>
         <div class="layout-main-container">
             <div class="layout-main">
+                <div>
+                    <Card>
+                        <template #title>Welcome to the Ignite Pickup Boxes control panel!</template>
+                        <template #content>
+                            <p class="m-0">
+                                Not sure where to start? Here's a quick guide:
+                            </p>
+                            <ol>
+                                <li>Open the <em>Makerspace Print Tracker</em> spreadsheet in a new tab</li>
+                                <li>Mark the finished print <em>In Boxes</em> on the sheet</li>
+                                <li>Copy the recipent's email address, and take note of the print number
+                                </li>
+                                <li>Click the <em>+ New</em> button below</li>
+                                <li>Type in the print number and paste the email address</li>
+                                <li>Type in a box number or click <em>✨ Auto</em></li>
+                                <li>Click <em>Save</em></li>
+                                <li>Click the blue unlock button next to the newly created print to open its box</li>
+                                <li>Place the print in the opened box, and close the door</li>
+                            </ol>
+                            <p class="m-0">
+                                This may seem like a lot of steps, but I wanted to be thorough in this documentation.
+                                It should only take a dozen seconds or so to complete each print delivery, and I'll
+                                remove or shrink this guide once everyone is comfortable with the process.
+                            </p>
+                            <p class="m-0">
+                                Note that this software automatically sends the recipent an email allowing them to
+                                unlock their box. If they have trouble, you can unlock it for them by clicking the blue
+                                unlock button next to their print.
+                            </p>
+                            <p class="m-0">
+                                By default, the below data table is sorted by print number, but you can click any column
+                                header to sort by that column instead. You can also search for a print number, email, or
+                                box number by typing in the search box above the table.
+                            </p>
+                            <p class="m-0">
+                                If you have any questions or suggestions, please don't hesitate to reach out to Sig
+                                while the bugs are being worked out. Thanks for your help!
+                            </p>
+                        </template>
+                    </Card>
+                </div>
                 <router-view></router-view>
             </div>
             <app-footer></app-footer>
