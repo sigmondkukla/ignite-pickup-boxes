51da92b5d12504459a87749517c0550925670fe3
Sigmond Kukla <sigkukla@gmail.com>
2024-10-29 11:44:17 -0400

    Add p wrapping ol

--- frontend/src/layout/AppLayout.vue
+++ frontend/src/layout/AppLayout.vue
@@ -16,6 +16,7 @@ import AppTopbar from './AppTopbar.vue';
                             <p class="m-0">
                                 Not sure where to start? Here's a quick guide:
                             </p>
+                            <p class="m-0">
                             <ol>
                                 <li>Open the <em>Makerspace Print Tracker</em> spreadsheet in a new tab</li>
                                 <li>Mark the finished print <em>In Boxes</em> on the sheet</li>
@@ -28,6 +29,7 @@ import AppTopbar from './AppTopbar.vue';
                                 <li>Click the blue unlock button next to the newly created print to open its box</li>
                                 <li>Place the print in the opened box, and close the door</li>
                             </ol>
+                            </p>
                             <p class="m-0">
                                 This may seem like a lot of steps, but I wanted to be thorough in this documentation.
                                 It should only take a dozen seconds or so to complete each print delivery, and I'll
