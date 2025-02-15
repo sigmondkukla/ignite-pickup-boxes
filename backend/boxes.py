a2836f8a5b1f80e93ba8713788b4042535472a17
Sigmond Kukla <sigkukla@gmail.com>
2025-02-15 17:27:25 -0500

    Create subclasses for types of boxes

--- backend/boxes.py
+++ backend/boxes.py
@@ -1,6 +1,7 @@
 # import shift_register
 import time
 import os
+import serial # for arduino-controlled boxes
 
 if os.name == "nt":
     import sys
@@ -11,7 +12,42 @@ if os.name == "nt":
 else:
     import RPi.GPIO as GPIO
 
-class Boxes():
+class Boxes(): # boxes parent class
+    def __init__(self, num_boxes): # constructor method
+        self.num_boxes = num_boxes
+        self.deactivate_all() # Ensure that no boxes just pop open randomly
+
+    def deactivate_all(self):
+        pass
+
+    def open_box(self, box_num):
+        pass
+
+    def cleanup(self):
+        pass
+
+class ArduinoBoxes(Boxes): # arduino-controlled boxes subclass
+    def __init__(self, num_boxes, serial_port): # constructor method for arduino-controlled boxes subclass
+        super().__init__(num_boxes) # call parent class constructor method
+        self.serial_port = serial_port
+        self.ser = serial.Serial(serial_port, 9600) # open serial port
+        self.deactivate_all() # Ensure that no boxes just pop open randomly
+
+    def deactivate_all(self):
+        pass # we assume that the arduino will handle this case
+
+    def open_box(self, box_num):
+        if box_num < 0:
+            raise ValueError("Box number must be greater than or equal to 0")
+        if box_num >= self.num_boxes:
+            raise ValueError(f"Box number must be less than the number of boxes ({self.num_boxes})")
+
+        self.ser.write(bytes([box_num + 1])) # send box number to arduino to open box
+
+    def cleanup(self):
+        self.ser.close() # close serial port
+
+class GPIOBoxes(Boxes): # GPIO-controlled boxes subclass
     def __init__(self, num_boxes, box_pins):
         self.num_boxes = num_boxes
         self.box_pins = box_pins
